1255bd9d87720c025e50b9889dc2fa9d8eac95ed
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-12-01 06:03:11 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,60 @@
+class Song
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  @@song_count_by_genre = {}
+  @@song_count_by_artist = {}
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+
+    if @@song_count_by_genre[genre] == nil || @@song_count_by_genre[genre] > @@count
+      @@song_count_by_genre[genre] = 0
+    end
+    @@song_count_by_genre[genre] += 1
+
+    if @@song_count_by_artist[artist] == nil
+      @@song_count_by_artist[artist] = 0
+    end
+    #@@song_count_by_artist[artist] += 1
+    puts "The songs by genre #{@@song_count_by_genre[genre]} and this many counts: #{@@count}"
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    unique_artists = []
+    @@artists.each do |artist|
+      if !unique_artists.include?(artist)
+        unique_artists << artist
+      end
+    end
+    unique_artists
+  end
+
+  def self.genres
+    unique_genres = []
+    @@genres.each do |genre|
+      if !unique_genres.include?(genre)
+        unique_genres << genre
+      end
+    end
+    unique_genres
+  end
+
+  def self.genre_count
+    @@song_count_by_genre
+  end
+
+  def self.artist_count
+    @@song_count_by_artist
+  end
+end
