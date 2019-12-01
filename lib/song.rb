1d3f4c69677511d997b1e003abb045da81ce927a
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-12-01 06:21:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,8 @@ class Song
     @@artists << artist
     @@genres << genre
 
-    if (@@song_count_by_genre[genre] == nil || @@song_count_by_genre[genre] > @@count) && @@song_count_by_genre.size != 1
+    if (@@song_count_by_genre[genre] == nil || @@song_count_by_genre[genre] > @@count)# && @@song_count_by_genre.size != 1
+      puts "#{song_count_by_genre.size}"
       @@song_count_by_genre[genre] = 1
     else
       @@song_count_by_genre[genre] += 1
